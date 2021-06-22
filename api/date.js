54ff697306708a62e0997ed5cc116cfcf3bbc51f
Aveek Saha <aveek.s98@gmail.com>
2021-06-22 12:55:54 +0530

    Access api

--- api/date.js
+++ api/date.js
@@ -1,4 +1,21 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+
 module.exports = (req, res) => {
   const date = new Date().toString();
-  res.status(200).send(date);
+  var scopes = ['user-read-private', 'user-read-email'],
+  redirectUri = 'http://localhost:3000',
+  clientId = '',
+  state = 'some-state-of-my-choice';
+
+  // Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
+  var spotifyApi = new SpotifyWebApi({
+      redirectUri: redirectUri,
+      clientId: clientId
+  });
+
+  // Create the authorization URL
+  var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+
+  console.log(authorizeURL);
+  res.status(200).send(authorizeURL);
 };
