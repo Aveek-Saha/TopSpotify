606e2483a952808cefe9619ee4413f24fc37b30e
Aveek Saha <aveek.s98@gmail.com>
2023-06-12 18:27:22 -0400

    Display track details

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Login from "@/components/auth/Login";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../pages/api/auth/[...nextauth]";
+import { signOut } from "next-auth/react";
 
 async function getTopSongs(accessToken: String | unknown) {
     const TOP_ENDPOINT =
-        "https://api.spotify.com/v1/me/top/tracks?time_range=long_term";
+        "https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=10";
 
     const topTracks = await fetch(TOP_ENDPOINT, {
         headers: {
@@ -19,6 +20,9 @@ export default async function Home() {
     const session = await getServerSession(authOptions);
     let topSongs = {};
     if (session) {
+        if (session.token.exp < Date.now() / 1000) {
+            return <Login/>
+        }
         topSongs = await getTopSongs(session.token.accessToken);
     }
 
@@ -26,7 +30,16 @@ export default async function Home() {
         <div>
             <h1>Hello World</h1>
             <Login />
-            {JSON.stringify(topSongs, null, 4)}
+            {topSongs.items.map((song) => {
+                return (
+                    <div>
+                        <img src={song.album.images[1].url}></img>
+                        <h3>{song.name}</h3>
+                        <p>{song.album.name}</p>
+                        <p>{(song.album.artists[0].name)}</p>
+                    </div>
+                )
+            })}
         </div>
     );
 }
