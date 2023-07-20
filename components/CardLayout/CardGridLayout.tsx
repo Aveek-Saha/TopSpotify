bbfbd13e4cba9780a8714225ed8e45c58e75a1bc
Aveek Saha <aveek.s98@gmail.com>
2023-07-20 17:35:05 -0400

    Style and layout changes

--- components/CardLayout/CardGridLayout.tsx
+++ components/CardLayout/CardGridLayout.tsx
@@ -6,8 +6,14 @@ export default function CardGridLayout({
     return (
         <>
             <div className="d-none d-sm-block d-md-block">
-                <div className="row row-cols-2 row-cols-xl-5 row-cols-lg-5 row-cols-md-4 row-cols-sm-3 row-cols-xs-2 g-4 justify-content-center mb-4">
-                    {children}
+                <div className="row">
+                    <div className="col-xl-1"></div>
+                    <div className="col">
+                        <div className="row row-cols-2 row-cols-xl-5 row-cols-lg-5 row-cols-md-4 row-cols-sm-3 row-cols-xs-2 g-3 justify-content-center mb-4">
+                            {children}
+                        </div>
+                    </div>
+                    <div className="col-xl-1"></div>
                 </div>
             </div>
         </>
--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -4,13 +4,18 @@ import { Suspense } from "react";
 import LoadingUser from "../loading/LoadingUser";
 
 import Logout from "../auth/Logout";
+import Link from "next/link";
 
 export default async function Navbar() {
     const session = await getServerSession(authOptions);
     return (
         <div className="row">
             <header className="header my-4 d-flex align-items-center">
-                <h1 className="header__title">Top Tunes Spotify</h1>
+                <h1 className="header__title">
+                    <Link href={"/"} className="link">
+                        Top Tunes Spotify
+                    </Link>
+                </h1>
                 <p className="header__item d-flex flex-wrap align-items-center m-0 ms-auto">
                     {session?.user && (
                         <Suspense fallback={<LoadingUser />}>
